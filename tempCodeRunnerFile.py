63e6eb0be62c9562540163415f0f22b398976966
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-10-09 20:31:36 +0200

    dxd

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
- print("---------------------------------------------------")
-        print("Final tax (Brutto):",final_tax)
-        print("Final tax (Netto):",final_tax_netto)
-        print("-----
